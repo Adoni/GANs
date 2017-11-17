2ff8940b62270968bffecf19ef3a69042b75b61b
Sun Xiaofei <adoni1203@gmail.com>
2017-11-16 20:56:30 -0500

    train D for 100 times

--- WGAN.py
+++ WGAN.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[21]:
+# In[1]:
 
 
 import torch
@@ -14,9 +14,11 @@ import torch.optim as optim
 import sys
 
 
-# In[22]:
+# In[2]:
 
 
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from torchvision import utils
 show_image=True
@@ -25,54 +27,58 @@ def imshow(inp, file_name, save=False, title=None):
     fig = plt.figure(figsize=(5, 5))
     inp = inp.numpy().transpose((1, 2, 0))
     plt.imshow(inp, cmap='gray')
+    plt.savefig(file_name)
     if show_image:
         plt.show()
+    else:
+        print("Don't show")
 
 
-# In[23]:
+# In[17]:
+
+
+z_size=128
+hidden_size=512
+img_size=28
+
+
+# In[19]:
 
 
 root = './data'
 download = True
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5,), (0.5,0.5,0.5))])
-trans = transforms.Compose([transforms.ToTensor()])
-train_set = dset.MNIST(root=root, train=True, transform=trans, download=download)
+trans = transforms.Compose([
+    transforms.Scale(img_size),
+    transforms.ToTensor(),
+    transforms.Normalize((
+        0.5,
+        0.5,
+        0.5,
+    ), (0.5, 0.5, 0.5))
+])
+train_set = dset.MNIST(
+    root=root, train=True, transform=trans, download=download)
 test_set = dset.MNIST(root=root, train=False, transform=trans)
 batch_size = 128
 kwargs = {}
 train_loader = torch.utils.data.DataLoader(
-                 dataset=train_set,
-                 batch_size=batch_size,
-                 shuffle=True)
+    dataset=train_set, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(
-                dataset=test_set,
-                batch_size=batch_size,
-                shuffle=False)
+    dataset=test_set, batch_size=batch_size, shuffle=False)
 
 
-# In[24]:
-
-
-z_size=128
-hidden_size=128
-img_size=28
-
-
-# In[28]:
+# In[20]:
 
 
 class Generator(nn.Module):
     def __init__(self):
         super(Generator,self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(z_size, hidden_size*2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(hidden_size*2, hidden_size*4),
-            nn.LeakyReLU(0.2, inplace=True),
-#             nn.Linear(hidden_size*4, hidden_size*8),
-#             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(hidden_size*4, img_size**2),
-            nn.Tanh()
+            nn.Linear(z_size, hidden_size),
+            nn.ReLU(True),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(True),
+            nn.Linear(hidden_size, img_size**2),
         )
     def forward(self, x):
         x = x.view(x.size()[0], z_size)
@@ -81,31 +87,33 @@ class Generator(nn.Module):
         return out
 
 
-# In[29]:
+# In[21]:
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator,self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(img_size**2, hidden_size*4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.3),
-#             nn.Linear(hidden_size*8, hidden_size*4),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Dropout(0.3),
-            nn.Linear(hidden_size*4, hidden_size*2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.3),
-            nn.Linear(hidden_size*2, 1),
+            nn.Linear(img_size**2, hidden_size),
+            nn.ReLU(True),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(True),
+            nn.Linear(hidden_size, 1),
         )
     def forward(self, x):
         out = self.model(x.view(x.size(0), img_size**2))
         out = out.view(out.size(0), -1)
-        return out
+        return torch.mean(out)
 
 
-# In[30]:
+# In[22]:
+
+
+one = torch.FloatTensor([1])
+mone = one * -1
+
+
+# In[ ]:
 
 
 from tqdm import tqdm
@@ -121,7 +129,9 @@ optimizers = {
 }
 criterion = nn.BCELoss()
 for epoch in tqdm(range(10000)):
-    for _ in range(5):
+    for p in D.parameters():
+        p.requires_grad = True
+    for _ in range(100):
         optimizers['D'].zero_grad()
         data=next(iter(train_loader))[0]
         if torch.cuda.is_available():
@@ -129,20 +139,24 @@ for epoch in tqdm(range(10000)):
         else:
             data = Variable(data)
         output_real = D(data)
+        output_real.backward(one)
         noisev = torch.randn(data.size()[0], z_size, 1, 1)
         if torch.cuda.is_available():
             noisev = Variable(noisev.cuda())
         else:
             noisev = Variable(noisev)
-        fake_data = G(noisev)
+        fake_data = Variable(G(noisev).data)
         output_fake = D(fake_data)
-        D_loss = -(torch.mean(output_real) - torch.mean(output_fake))
+        output_fake.backward(mone)
+#         D_loss = -(torch.mean(output_real) - torch.mean(output_fake))
 
-        D_loss.backward()
+#         D_loss.backward()
         optimizers['D'].step()
         for p in D.parameters():
             p.data.clamp_(-0.01, 0.01)
 
+    for p in D.parameters():
+        p.requires_grad = False
     optimizers['G'].zero_grad()
     noisev = torch.randn(data.size()[0], z_size, 1, 1)
     if torch.cuda.is_available():
@@ -151,46 +165,19 @@ for epoch in tqdm(range(10000)):
         noisev = Variable(noisev)
     fake_data = G(noisev)
     output_fake1 = D(fake_data)
-    G_loss = -torch.mean(output_fake1)
+    output_fake1.backward(one)
+#     G_loss = -torch.mean(output_fake1)
 
-    G_loss.backward()
+#     G_loss.backward()
     optimizers['G'].step()
 
     if epoch % 1000 == 0:
+        print('show')
         if torch.cuda.is_available():
             dd = utils.make_grid(fake_data.cpu().data[:16])
         else:
             dd = utils.make_grid(fake_data.data[:16])
         imshow(dd,'./results/WGAN_%d.png'%(epoch))
-
-
-# In[28]:
-
-
-class Generator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        d=128
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(z_size, d*8, 4, 1, 0),
-            nn.BatchNorm2d(d*8),
-            nn.ReLU(),
-            nn.ConvTranspose2d(d*8, d*4, 4, 2, 1),
-            nn.BatchNorm2d(d*4),
-            nn.ReLU(),
-            nn.ConvTranspose2d(d*4, d*2, 4, 2, 1),
-            nn.BatchNorm2d(d*2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(d*2, d, 4, 2, 1),
-            nn.BatchNorm2d(d),
-            nn.ReLU(),
-            nn.ConvTranspose2d(d, 1, 4, 2, 1),
-            nn.Tanh(),
-        )
-    def forward(self, x):
-        x = x.view(x.size()[0], z_size, 1,1)
-        out = self.model(x)
-        print(out.size())
-        out = out.view(x.size()[0], 1,img_size,img_size)
-        return out
+#         dd = utils.make_grid(data.data[:16])
+#         imshow(dd,'./results/WGAN_%d.png'%(epoch))
 
