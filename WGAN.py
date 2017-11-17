b434237db7cadeabab50fc744d7102cb8847ed8b
Sun Xiaofei <adoni1203@gmail.com>
2017-11-17 14:06:22 -0500

    find error

--- WGAN.py
+++ WGAN.py
@@ -11,6 +11,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision.utils as vutils
 import sys
 
 
@@ -25,10 +26,10 @@ from torchvision import utils
 show_image=False
 def imshow(inp, file_name, save=False, title=None):
     """Imshow for Tensor."""
-    fig = plt.figure(figsize=(5, 5))
+    fig = plt.figure(figsize=(10, 10))
     inp = inp.numpy().transpose((1, 2, 0))
-    inp-=numpy.min(inp)
-    plt.imshow(inp, cmap='gray')
+#     inp-=numpy.min(inp)
+    plt.imshow(inp)
     plt.savefig(file_name)
     if show_image:
         plt.show()
@@ -52,11 +53,7 @@ download = True
 trans = transforms.Compose([
     transforms.Scale(img_size),
     transforms.ToTensor(),
-    transforms.Normalize((
-        0.5,
-        0.5,
-        0.5,
-    ), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 train_set = dset.MNIST(
     root=root, train=True, transform=trans, download=download)
@@ -104,17 +101,34 @@ class Discriminator(nn.Module):
         )
     def forward(self, x):
         out = self.model(x.view(x.size(0), img_size**2))
-        out = out.view(out.size(0), -1)
-        return torch.mean(out)
+        out=out.mean(0)
+        out = out.view(1)
+        return out
 
 
-# In[19]:
+# In[16]:
 
 
 one = torch.FloatTensor([1])
+noise_holder=torch.FloatTensor(batch_size, z_size, 1, 1)
+input_holder = torch.FloatTensor(batch_size, 1, img_size, img_size)
 if torch.cuda.is_available():
     one=one.cuda()
+    noise_holder.cuda()
+    input_holder.cuda()
 mone = one * -1
+
+
+# In[17]:
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
 
 
 # In[18]:
@@ -122,6 +136,7 @@ mone = one * -1
 
 from tqdm import tqdm
 G = Generator()
+G.apply(weights_init)
 D = Discriminator()
 if torch.cuda.is_available():
     G.cuda()
@@ -132,45 +147,35 @@ optimizers = {
     'G': torch.optim.RMSprop(G.parameters(), lr=G_lr)
 }
 criterion = nn.BCELoss()
+err_Ds=[]
 for epoch in tqdm(range(10000)):
     for p in D.parameters():
         p.requires_grad = True
-    if epoch<200 or epoch%200==0:
+    if epoch<25 or epoch%100==0:
         iter_D=100
     else:
-        iter_D=5
+        iter_D=10
     for _ in range(iter_D):
+        for p in D.parameters():
+            p.data.clamp_(-0.01, 0.01)
         optimizers['D'].zero_grad()
         data=next(iter(train_loader))[0]
-        if torch.cuda.is_available():
-            data = Variable(data.cuda())
-        else:
-            data = Variable(data)
-        output_real = D(data)
+        input_holder.resize_as_(data).copy_(data)
+        output_real = D(Variable(data))
         output_real.backward(one)
-        noisev = torch.randn(data.size()[0], z_size, 1, 1)
-        if torch.cuda.is_available():
-            noisev = Variable(noisev.cuda())
-        else:
-            noisev = Variable(noisev)
+        noise_holder.resize_(data.size()[0], z_size, 1, 1).normal_(0, 1)
+        noisev = Variable(noise_holder,volatile=True)
         fake_data = Variable(G(noisev).data)
         output_fake = D(fake_data)
         output_fake.backward(mone)
-#         D_loss = -(torch.mean(output_real) - torch.mean(output_fake))
-
-#         D_loss.backward()
+        err_Ds.append((output_real+output_fake).data.numpy()[0])
         optimizers['D'].step()
-        for p in D.parameters():
-            p.data.clamp_(-0.01, 0.01)
 
     for p in D.parameters():
         p.requires_grad = False
     optimizers['G'].zero_grad()
-    noisev = torch.randn(data.size()[0], z_size, 1, 1)
-    if torch.cuda.is_available():
-        noisev = Variable(noisev.cuda())
-    else:
-        noisev = Variable(noisev)
+    noise_holder.resize_(data.size()[0], z_size, 1, 1).normal_(0, 1)
+    noisev = Variable(noise_holder)
     fake_data = G(noisev)
     output_fake1 = D(fake_data)
     output_fake1.backward(one)
@@ -186,6 +191,23 @@ for epoch in tqdm(range(10000)):
         else:
             dd = utils.make_grid(fake_data.data[:16])
         imshow(dd,'./results/WGAN_%d.png'%(epoch))
-#         dd = utils.make_grid(data.data[:16])
-#         imshow(dd,'./results/WGAN_%d.png'%(epoch))
+        dd = dd.mul(0.5).add(0.5)
+        vutils.save_image(dd, './results/OrWGAN_%d.png'%(epoch))
+        plt.plot(err_Ds)
+        plt.show()
+
+
+# In[60]:
+
+
+import torchvision.utils as vutils
+data=next(iter(train_loader))[0]
+data = data.mul(0.5).add(0.5)
+vutils.save_image(data, 'my.png')
+
+
+# In[72]:
+
+
+err_Ds[0].data.numpy()[0]
 
