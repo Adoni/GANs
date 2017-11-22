ab7a448b479843a8289ba5322c63df2e4d02cce7
Sun Xiaofei <adoni1203@gmail.com>
2017-11-21 19:54:06 -0500

    use xuan's code

--- sinkhorn.py
+++ sinkhorn.py
@@ -1,25 +1,240 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+from __future__ import print_function
+import random
 import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
 from torch.autograd import Variable
+import numpy as np
+from IPython.core.debugger import Tracer
+import matplotlib.pyplot as plt
+from torchvision import utils
 
 
-def sinkhorn_normalized(x, y, epsilon, n, niter):
-    Wxy = sinkhorn_loss(x, y, epsilon, n, niter)
-    Wxx = sinkhorn_loss(x, x, epsilon, n, niter)
-    Wyy = sinkhorn_loss(y, y, epsilon, n, niter)
-    return 2 * Wxy - Wxx - Wyy
+# In[3]:
+
+
+def imshow(inp, save=False, title=None):
+    """Imshow for Tensor."""
+    fig = plt.figure(figsize=(5, 5))
+    inp = transforms.ToPILImage()(inp)
+    plt.imshow(inp,cmap='gray')
+    plt.show()
+
+
+# In[4]:
+
+
+class MLP_G(nn.Module):
+    def __init__(self, isize, nz, nc, ngf, ngpu):
+        super(MLP_G, self).__init__()
+        self.ngpu = ngpu
+
+        main = nn.Sequential(
+            # Z goes into a linear of size: ngf
+            nn.Linear(nz, ngf),
+            #nn.ReLU(True),
+            #nn.Linear(ngf, ngf),
+            #nn.ReLU(True),
+            #nn.Linear(ngf, ngf),
+            nn.ReLU(True),
+            nn.Linear(ngf, nc * isize * isize),
+        )
+        self.main = main
+        self.nc = nc
+        self.isize = isize
+        self.nz = nz
+
+    def forward(self, input):
+        input = input.view(input.size(0), input.size(1))
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output.view(output.size(0), self.nc, self.isize, self.isize)
+
+
+# In[5]:
+
+
+class MLP_D(nn.Module):
+    def __init__(self, isize, nz, nc, ndf, ngpu):
+        super(MLP_D, self).__init__()
+        self.ngpu = ngpu
+
+        main = nn.Sequential(
+            # Z goes into a linear of size: ndf
+            nn.Linear(nc * isize * isize, ndf),
+            nn.ReLU(True),
+            nn.Linear(ndf, ndf),
+            nn.ReLU(True),
+            nn.Linear(ndf, ndf),
+            nn.ReLU(True),
+            nn.Linear(ndf, 1),
+        )
+        self.main = main
+        self.nc = nc
+        self.isize = isize
+        self.nz = nz
+
+    def forward(self, input):
+        input = input.view(input.size(0),
+                           input.size(1) * input.size(2) * input.size(3))
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        output = output.mean(0)
+        return output.view(1)
+
+
+# In[6]:
+
+
+class DCGAN_D(nn.Module):
+    def __init__(self, isize, nz, nc, ndf, ngpu, n_extra_layers=0):
+        super(DCGAN_D, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        main = nn.Sequential()
+        # input is nc x isize x isize
+        main.add_module('initial.conv.{0}-{1}'.format(nc, ndf),
+                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
+        main.add_module('initial.relu.{0}'.format(ndf),
+                        nn.LeakyReLU(0.2, inplace=True))
+        csize, cndf = isize / 2, ndf
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}.{1}.conv'.format(t, cndf),
+                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}.{1}.batchnorm'.format(t, cndf),
+                            nn.BatchNorm2d(cndf))
+            main.add_module('extra-layers-{0}.{1}.relu'.format(t, cndf),
+                            nn.LeakyReLU(0.2, inplace=True))
+
+        while csize > 4:
+            in_feat = cndf
+            out_feat = cndf * 2
+            main.add_module('pyramid.{0}-{1}.conv'.format(in_feat, out_feat),
+                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+            main.add_module('pyramid.{0}.batchnorm'.format(out_feat),
+                            nn.BatchNorm2d(out_feat))
+            main.add_module('pyramid.{0}.relu'.format(out_feat),
+                            nn.LeakyReLU(0.2, inplace=True))
+            cndf = cndf * 2
+            csize = csize / 2
+
+        # state size. K x 4 x 4
+        main.add_module('final.{0}-{1}.conv'.format(cndf, 1),
+                        nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
+        self.main = main
+
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else: 
+            output = self.main(input)
+            
+        output = output.mean(0)
+        return output.view(1)
+
+
+# In[7]:
+
+
+class DCGAN_G(nn.Module):
+    def __init__(self, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
+        super(DCGAN_G, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        cngf, tisize = ngf//2, 4
+        while tisize != isize:
+            cngf = cngf * 2
+            tisize = tisize * 2
+
+        main = nn.Sequential()
+        # input is Z, going into a convolution
+        main.add_module('initial.{0}-{1}.convt'.format(nz, cngf),
+                        nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
+        main.add_module('initial.{0}.batchnorm'.format(cngf),
+                        nn.BatchNorm2d(cngf))
+        main.add_module('initial.{0}.relu'.format(cngf),
+                        nn.ReLU(True))
+
+        csize, cndf = 4, cngf
+        while csize < isize//2:
+            main.add_module('pyramid.{0}-{1}.convt'.format(cngf, cngf//2),
+                            nn.ConvTranspose2d(cngf, cngf//2, 4, 2, 1, bias=False))
+            main.add_module('pyramid.{0}.batchnorm'.format(cngf//2),
+                            nn.BatchNorm2d(cngf//2))
+            main.add_module('pyramid.{0}.relu'.format(cngf//2),
+                            nn.ReLU(True))
+            cngf = cngf // 2
+            csize = csize * 2
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}.{1}.conv'.format(t, cngf),
+                            nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}.{1}.batchnorm'.format(t, cngf),
+                            nn.BatchNorm2d(cngf))
+            main.add_module('extra-layers-{0}.{1}.relu'.format(t, cngf),
+                            nn.ReLU(True))
+
+        main.add_module('final.{0}-{1}.convt'.format(cngf, nc),
+                        nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
+        main.add_module('final.{0}.tanh'.format(nc),
+                        nn.Tanh())
+        self.main = main
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else: 
+            output = self.main(input)
+        return output 
+
+
+# In[8]:
+
+
+def cost_matrix(x, y,p=2) :
+    "Returns the matrix of $|x_i-y_j|^p$."
+    x_col = x.unsqueeze(1) 
+    y_lin = y.unsqueeze(0)
+    c = torch.sum( (torch.abs(x_col - y_lin))**p , 2) 
+    return c 
+
+
+# In[132]:
 
 
 def sinkhorn_loss(x, y, epsilon, n, niter):
     """
-	Given two emprical measures with n points each with locations x and y
-	outputs an approximation of the OT cost with regularization parameter epsilon
-	niter is the max. number of steps in sinkhorn loop
-	"""
+    Given two emprical measures with n points each with locations x and y
+    outputs an approximation of the OT cost with regularization parameter epsilon
+    niter is the max. number of steps in sinkhorn loop
+    """
     # The Sinkhorn algorithm takes as input three variables :
-    C = cost_matrix(x, y)  # Wasserstein cost function
+    C = cost_matrix(x, y)/n  # Wasserstein cost function
 
     # both marginals are fixed with equal weights
-    if torch.cuda.is_available():
+    if cuda:
         mu = Variable(
             1. / n * torch.cuda.FloatTensor(n).fill_(1), requires_grad=False)
         nu = Variable(
@@ -69,15 +284,186 @@ def sinkhorn_loss(x, y, epsilon, n, niter):
             break
     U, V = u, v
     pi = torch.exp(M(U, V))  # Transport plan pi = diag(a)*K*diag(b)
-    # cost = torch.sum(Gamma * C)  # Sinkhorn cost
-    cost = torch.sum(C)
-
+    cost = torch.sum(pi * C)  # Sinkhorn cost
     return cost
 
 
-def cost_matrix(x, y, p=2):
-    "Returns the matrix of $|x_i-y_j|^p$."
-    x_col = x.unsqueeze(1)
-    y_lin = y.unsqueeze(0)
-    c = torch.sum((torch.abs(x_col - y_lin))**p, 2)
-    return c
+# In[130]:
+
+
+def sinkhorn_normalized(x,y,epsilon,n,niter):
+
+    x = x.view(x.size(0), x.size(1) * x.size(2) * x.size(3))
+    y = y.view(y.size(0), y.size(1) * y.size(2) * y.size(3))
+    Wxy = sinkhorn_loss(x,y,epsilon,n,niter)
+    Wxx = sinkhorn_loss(x,x,epsilon,n,niter)
+    Wyy = sinkhorn_loss(y,y,epsilon,n,niter)
+    return 2*Wxy - Wxx - Wyy 
+
+
+# In[146]:
+
+
+ngpu = 1 # number of GPUs to use
+nz = 2 # size of the latent z vector
+ngf = 500
+ndf = 500
+nc = 1 # input image channels
+n_extra_layers = 0 # Number of extra layers on gen and disc
+
+imageSize = 28
+batchSize = 100
+n_workers = 2
+
+
+adam = False
+lrG = 0.005
+
+beta1 = 0.5 # beta1 for adam. default=0.5
+niter = 100 # number of epochs to train for
+
+clamp_lower = -0.1
+clamp_upper = 0.1
+
+experiment = './experiment' # Where to store samples and models
+
+epsilon = .1 # panalty weight
+L = 100 # sinkhorn iteration num
+
+netG_path = experiment + '/netG_sinkhorn.pth'
+netD_path = ''
+
+cuda = True
+
+
+# In[136]:
+
+
+# get data
+dataset = dset.MNIST(root='./data', download=True,
+                           transform=transforms.Compose([
+                               transforms.Scale(imageSize),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batchSize,
+                                         shuffle=True, num_workers=n_workers)
+
+
+# In[137]:
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+# In[138]:
+
+
+netG = MLP_G(imageSize, nz, nc, ngf, ngpu)
+
+netG.apply(weights_init)
+try: # load checkpoint if needed
+    netG.load_state_dict(torch.load(netG_path))
+    print("load parameters")
+except:
+    pass
+print(netG)
+
+
+# In[139]:
+
+
+input = torch.FloatTensor(batchSize, 3, imageSize, imageSize)
+noise = torch.FloatTensor(batchSize, nz, 1, 1)
+fixed_noise = torch.FloatTensor(batchSize, nz, 1, 1).normal_(0, 1)
+one = torch.FloatTensor([1])
+mone = one * -1
+
+
+# In[140]:
+
+
+if cuda and torch.cuda.is_available():
+    netG.cuda()
+    input = input.cuda()
+    one, mone = one.cuda(), mone.cuda()
+    noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+
+
+# In[141]:
+
+
+if adam:
+    optimizerG = optim.Adam(netG.parameters(), lr=lrG, betas=(beta1, 0.999))
+else:
+    optimizerG = optim.RMSprop(netG.parameters(), lr = lrG)
+
+
+# In[ ]:
+
+
+
+gen_iterations = 0
+for epoch in range(niter):
+    
+    data_iter = iter(dataloader)
+    i = 0
+    while i < len(dataloader):
+        
+        ############################
+        # (2) Update G network
+        ###########################
+        data = data_iter.next()
+        real_cpu, _ = data
+        if cuda and torch.cuda.is_available():
+            real_cpu = real_cpu.cuda()
+        netG.zero_grad()
+        # in case our last batch was the tail batch of the dataloader,
+        # make sure we feed a full batch of noise
+        noise.resize_(batchSize, nz).normal_(0, 1)
+        if cuda and torch.cuda.is_available():
+            noise = noise.cuda()
+        noisev = Variable(noise, requires_grad = False)
+        fake = netG(noisev)
+        #Tracer()()
+        input.resize_as_(real_cpu).copy_(real_cpu)
+        inputv = Variable(input, requires_grad = False)
+        loss = sinkhorn_normalized(fake, inputv, epsilon, batchSize, L) #- compute_sinkhorn_loss(batchSize, fake, fake, epsilon, L) - compute_sinkhorn_loss(batchSize, inputv, inputv, epsilon, L)
+        loss.backward(one)
+        optimizerG.step()
+        gen_iterations += 1
+        
+        if gen_iterations % 100== 0:
+            print('[%d/%d][%d/%d][%d] Sinkhorn_Loss: %f'
+            % (epoch, niter, i, len(dataloader), gen_iterations,
+            loss.data[0]))
+            real_cpu = real_cpu.mul(0.5).add(0.5)
+            vutils.save_image(real_cpu, '{0}/real_samples_sinkhorn.png'.format(experiment))
+            fake = netG(Variable(fixed_noise, volatile=True))
+            fake.data = fake.data.mul(0.5).add(0.5)
+            vutils.save_image(fake.data, '{0}/fake_samples_sinkhorn.png'.format(experiment))
+            if cuda and torch.cuda.is_available():
+                dd = utils.make_grid(fake.cpu().data[:16])
+            else:
+                dd = utils.make_grid(fake.data[:16]) 
+            imshow(dd)
+            # do checkpointing"
+            torch.save(netG.state_dict(), '{0}/netG_sinkhorn.pth'.format(experiment))
+        i += 1
+
+    
+
+
+# In[ ]:
+
+
+
+
