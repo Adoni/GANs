4e0511dfc365dc7661c62366dcd36808068f1a64
Sun Xiaofei <adoni1203@gmail.com>
2017-11-30 00:18:47 -0500

    add sinkhorn dc

--- SinkhornGAN-DC.py
+++ SinkhornGAN-DC.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[23]:
+# In[1]:
 
 
 import torch
@@ -13,110 +13,81 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.utils as vutils
 import sys
-import matplotlib
-import numpy
-import os
-if 'DISPLAY' not in os.environ:
-    matplotlib.use('Agg')
-    show_image=False
-else:
-    show_image=True
-from matplotlib import pyplot as plt
+from utils.show_image import imshow
 from torchvision import utils
+from models import Sinkhorn_DCGAN_D,DCGAN_G
+from utils.sinkhorn_loss import sinkhorn_loss
 
 
-# In[24]:
+# In[14]:
 
 
-def imshow(inp, file_name, save=False, title=None):
-    """Imshow for Tensor."""
-    fig = plt.figure(figsize=(5, 5))
-    inp = transforms.ToPILImage()(inp)
-    plt.imshow(inp,cmap='gray')
-    plt.savefig(file_name)
-    if show_image:
-        plt.show()
-    else:
-        plt.gcf().clear()
-
-
-# In[25]:
-
-
-batch_size = 100
-z_size=2
+z_size=100
 hidden_size=64
-img_size=32
-niter=100
+batch_size = 64
+dataset_name="MNIST"
+output_dimension=100
 epsilon=0.1
-G_lr = D_lr = 5e-3
+niter=100
 use_cuda=torch.cuda.is_available()
+print('Use cuda: %r'%use_cuda)
 
 
-# In[26]:
+# In[15]:
 
 
-root = './data'
-download = True
-trans = transforms.Compose([
-    transforms.Scale(img_size),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-data_set = dset.MNIST(
-    root=root, train=True, transform=trans, download=download)
-kwargs = {}
-D_loader = torch.utils.data.DataLoader(
-    dataset=data_set, batch_size=batch_size, shuffle=True)
-G_loader = torch.utils.data.DataLoader(
-    dataset=data_set, batch_size=batch_size, shuffle=True)
+if dataset_name == 'MNIST':
+    total_epoch=10000
+    img_size=32
+    image_chanel = 1
+    model_name = 'WGAN_DC_MNIST'
+    root = './data/mnist/'
+    download = True
+    trans = transforms.Compose([
+        transforms.Scale(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    data_set = dset.MNIST(
+        root=root, transform=trans, download=download)
+if dataset_name == "LSUN":
+    total_epoch=100000
+    img_size=64
+    image_chanel = 3
+    model_name = 'WGAN_DC_LSUN'
+    root = './data/lsun/'
+    trans = transforms.Compose([
+        transforms.Scale(img_size),
+        transforms.CenterCrop(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+    data_set = dset.LSUN(
+        db_path=root, classes=['bedroom_train'], transform=trans)
+if dataset_name == 'CIFAR':
+    total_epoch=10000
+    img_size=32
+    image_chanel = 1
+    model_name = 'WGAN_DC_CIFAR'
+    root = './data/cifar10/'
+    download = True
+    trans = transforms.Compose([
+        transforms.Scale(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    data_set = dset.MNIST(
+        root=root, transform=trans, download=download)
 
 
-# In[27]:
+# In[16]:
 
 
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator,self).__init__()
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(z_size,hidden_size*8,4,2,1),
-            nn.BatchNorm2d(hidden_size*8),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(hidden_size*8,hidden_size*4,4,2,1),
-            nn.BatchNorm2d(hidden_size*4),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(hidden_size*4,hidden_size*2,4,2,1),
-            nn.BatchNorm2d(hidden_size*2),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(hidden_size*2,hidden_size,4,2,1),
-            nn.BatchNorm2d(hidden_size*1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(hidden_size,1,4,2,1),
-            nn.Tanh(),
-        )
-    def forward(self, x):
-        x = x.view(x.size()[0], z_size, 1, 1)
-        out = self.model(x)
-        out = out.view(x.size()[0], 1, img_size,img_size)
-        return out
+data_loader = torch.utils.data.DataLoader(
+        dataset=data_set, batch_size=batch_size, shuffle=True)
 
 
-# In[28]:
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.main=nn.Sequential(
-            nn.Linear(100,100)
-        )
-
-    def forward(self, x):
-        x = x.view(x.size(0), img_size * img_size)
-        return x
-
-
-# In[29]:
+# In[19]:
 
 
 one = torch.FloatTensor([1])
@@ -129,7 +100,7 @@ if use_cuda:
 mone = one * -1
 
 
-# In[30]:
+# In[20]:
 
 
 def weights_init(m):
@@ -141,129 +112,87 @@ def weights_init(m):
         m.bias.data.fill_(0)
 
 
-# In[31]:
-
-
-def sinkhorn_loss(x, y, epsilon, n, niter):
-    """
-	Given two emprical measures with n points each with locations x and y
-	outputs an approximation of the OT cost with regularization parameter epsilon
-	niter is the max. number of steps in sinkhorn loop
-	"""
-    # The Sinkhorn algorithm takes as input three variables :
-    C = cost_matrix(x, y)/n  # Wasserstein cost function
-
-    # both marginals are fixed with equal weights
-    if use_cuda:
-        mu = Variable(
-            1. / n * torch.cuda.FloatTensor(n).fill_(1), requires_grad=False)
-        nu = Variable(
-            1. / n * torch.cuda.FloatTensor(n).fill_(1), requires_grad=False)
-    else:
-        mu = Variable(
-            1. / n * torch.FloatTensor(n).fill_(1), requires_grad=False)
-        nu = Variable(
-            1. / n * torch.FloatTensor(n).fill_(1), requires_grad=False)
-
-    # Parameters of the Sinkhorn algorithm.
-    rho = 1  #(.5) **2          # unbalanced transport
-    tau = -.8  # nesterov-like acceleration
-    lam = rho / (rho + epsilon)  # Update exponent
-    thresh = 10**(-1)  # stopping criterion
-
-    # Elementary operations .....................................................................
-    def ave(u, u1):
-        "Barycenter subroutine, used by kinetic acceleration through extrapolation."
-        return tau * u + (1 - tau) * u1
-
-    def M(u, v):
-        "Modified cost for logarithmic updates"
-        "$M_{ij} = (-c_{ij} + u_i + v_j) / \epsilon$"
-        return (-C + u.unsqueeze(1) + v.unsqueeze(0)) / epsilon
-
-    def lse(A):
-        "log-sum-exp"
-        return torch.log(torch.exp(A).sum(1, keepdim=True) +
-                         1e-6)  # add 10^-6 to prevent NaN
-
-    # Actual Sinkhorn loop ......................................................................
-    u, v, err = 0. * mu, 0. * nu, 0.
-    actual_nits = 0  # to check if algorithm terminates because of threshold or max iterations reached
-
-    for i in range(niter):
-        u1 = u  # useful to check the update
-        u = epsilon * (torch.log(mu) - lse(M(u, v)).squeeze()) + u
-        v = epsilon * (torch.log(nu) - lse(M(u, v).t()).squeeze()) + v
-        # accelerated unbalanced iterations
-        #u = ave( u, lam * ( epsilon * ( torch.log(mu.unsqueeze(1)) - lse(M(u,v))   ) + u ) )
-        #v = ave( v, lam * ( epsilon * ( torch.log(nu.unsqueeze(1)) - lse(M(u,v).t()) ) + v ) )
-        err = (u - u1).abs().sum()
-
-        actual_nits += 1
-        if (err < thresh).data.cpu().numpy():
-            break
-    U, V = u, v
-    pi = torch.exp(M(U, V))  # Transport plan pi = diag(a)*K*diag(b)
-    cost = torch.sum(pi * C)  # Sinkhorn cost
-
-    return cost
-
-
-def cost_matrix(x, y, p=2):
-    "Returns the matrix of $|x_i-y_j|^p$."
-    x_col = x.unsqueeze(1)
-    y_lin = y.unsqueeze(0)
-    c = torch.sum((torch.abs(x_col - y_lin))**p, 2)
-    return c
-
-
-# In[32]:
+# In[23]:
 
 
 from tqdm import tqdm
-G = Generator()
+G = DCGAN_G(isize=img_size, nz=z_size, nc=image_chanel, ngf=hidden_size, ngpu=0)
 G.apply(weights_init)
-D = Discriminator()
+D = Sinkhorn_DCGAN_D(isize=img_size, nz=z_size, nc=image_chanel, ndf=hidden_size, ngpu=0, output_dimension=output_dimension)
+D.apply(weights_init)
 print(G)
+print(D)
 if use_cuda:
     G.cuda()
     D.cuda()
+G_lr = D_lr = 5e-5
 optimizers = {
     'D': torch.optim.RMSprop(D.parameters(), lr=D_lr),
     'G': torch.optim.RMSprop(G.parameters(), lr=G_lr)
 }
-criterion = nn.BCELoss()
-errs=[]
-for epoch in tqdm(range(10000)):
-    optimizers['G'].zero_grad()
-    data=next(iter(G_loader))[0]
-    if use_cuda:
-        data=data.cuda()
-    input_holder.resize_as_(data).copy_(data)
-    y = D(Variable(data))
-    noise_holder.resize_(data.size()[0], z_size, 1, 1).normal_(0, 1)
-    noisev = Variable(noise_holder)
-    fake_data = G(noisev)
-    x = D(fake_data)
-    W_xy=sinkhorn_loss(x,y,epsilon,batch_size,niter)
-    W_xx=sinkhorn_loss(x,x,epsilon,batch_size,niter)
-    W_yy=sinkhorn_loss(y,y,epsilon,batch_size,niter)
-    Loss_G=2*W_xy-W_xx-W_yy
-    Loss_G.backward(one)
-    if use_cuda:
-        errs.append(Loss_G.cpu().data[0])
-    else:
-        errs.append(Loss_G.data[0])
-    optimizers['G'].step()
+data_iter=iter(data_loader)
 
-    if epoch % 100 == 0:
-        if use_cuda:
-            dd = utils.make_grid(fake_data.cpu().data[:64])
+def training():
+    for epoch in tqdm(range(total_epoch)):
+        for p in D.parameters():
+            p.requires_grad = True
+        if epoch<25 or epoch%500==0:
+            iter_D=100
         else:
-            dd = utils.make_grid(fake_data.data[:64])
-        dd = dd.mul(0.5).add(0.5)
-        vutils.save_image(dd, './results/Vuil_Sinkhorn_DC_%d.png'%(epoch))
-        imshow(dd,'./results/Sinkhorn_DC_%d.png'%(epoch))
-        plt.plot(errs)
-        plt.savefig('./results/Sinkhorn_DC_loss.png')
+            iter_D=5
+        for _ in range(iter_D):
+            for p in D.parameters():
+                p.data.clamp_(-0.01, 0.01)
+            optimizers['D'].zero_grad()
+            try:
+                data=data_iter.next()[0]
+            except:
+                data_iter=iter(data_loader)
+                data=data_iter.next()[0]
+            if use_cuda:
+                data=data.cuda()
+            input_holder.resize_as_(data).copy_(data)
+            Y = D(Variable(data))
+            noise_holder.resize_(data.size()[0], z_size, 1, 1).normal_(0, 1)
+            noisev = Variable(noise_holder,volatile=True)
+            fake_data = Variable(G(noisev).data)
+            X = D(fake_data)
+            XY=sinkhorn_loss(X,Y,epsilon,batch_size,niter,use_cuda)
+            XX=sinkhorn_loss(X,X,epsilon,batch_size,niter,use_cuda)
+            YY=sinkhorn_loss(Y,Y,epsilon,batch_size,niter,use_cuda)
+            loss_real=2*XY-XX-YY
+            loss_real.backward(mone)
+            optimizers['D'].step()
+
+        for p in D.parameters():
+            p.requires_grad = False
+        optimizers['G'].zero_grad()
+        try:
+            data=data_iter.next()[0]
+        except:
+            data_iter=iter(data_loader)
+            data=data_iter.next()[0]
+        if use_cuda():
+            data=data.cuda()
+        input_holder.resize_as_(data).copy_(data)
+        Y = D(Variable(data))
+        noise_holder.resize_(data.size()[0], z_size, 1, 1).normal_(0, 1)
+        noisev = Variable(noise_holder)
+        fake_data = G(noisev)
+        X = D(fake_data)
+        XY=sinkhorn_loss(X,Y,epsilon,batch_size,niter,use_cuda)
+        XX=sinkhorn_loss(X,X,epsilon,batch_size,niter,use_cuda)
+        YY=sinkhorn_loss(Y,Y,epsilon,batch_size,niter,use_cuda)
+        loss_fake=2*XY-XX-YY
+        loss_fake.backward(one)
+        optimizers['G'].step()
+
+        if epoch % 1000 == 0:
+            if use_cuda():
+                dd = utils.make_grid(fake_data.cpu().data[:64])
+            else:
+                dd = utils.make_grid(fake_data.data[:64])
+            dd = dd.mul(0.5).add(0.5)
+            vutils.save_image(dd, './results/%s_%d.png'%(model_name,epoch))
+training()
 
